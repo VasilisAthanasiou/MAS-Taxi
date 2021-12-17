a0f3defe6f5886d3db23d20550c750cf25338689
Vasilis Athanasiou <Bill Athanasiou>
2021-12-17 22:41:31 +0200

    Added classes dir and updated jade.jar

--- src/corepack/TaxiAgent.java
+++ src/corepack/TaxiAgent.java
@@ -1,5 +1,10 @@
 package corepack;
+import jade.core.AID;
+import jade.core.Agent;
 
-
-public class TaxiAgent {
+public class TaxiAgent extends Agent {
+    protected void setup(){
+        System.out.println("Taxi agent "+getAID().getName()+" is ready.");
+        this.doDelete();
+    }
 }
